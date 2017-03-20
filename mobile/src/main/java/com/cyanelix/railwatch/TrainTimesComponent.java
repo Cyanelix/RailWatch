b9d3cf61d3ec17ac54fe2599f974ef9531afcc33
Dave Payne <david.payne@multicom.co.uk>
2017-03-20 06:53:45 +0000

    Remove unused code

--- mobile/src/main/java/com/cyanelix/railwatch/TrainTimesComponent.java
+++ mobile/src/main/java/com/cyanelix/railwatch/TrainTimesComponent.java
@@ -1,12 +1,10 @@
 package com.cyanelix.railwatch;
 
-import com.cyanelix.railwatch.service.times.TrainTimesClient;
 import com.cyanelix.railwatch.service.times.TrainTimesService;
 
 import dagger.Component;
 
 @Component
 public interface TrainTimesComponent {
-    TrainTimesService trainTimesService();
     void inject(MainActivity mainActivity);
 }
